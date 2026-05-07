15db4c6a8d28da408ded2152ca4d2924eb5f75d9
rodrigo <rodrigotoctoportocarrero@gmail.com>
2026-05-06 19:00:07 -0500

    se arreglo algunas cosas de diseño

--- capaPresentacion/InicioSesion.Designer.vb
+++ capaPresentacion/InicioSesion.Designer.vb
@@ -22,96 +22,110 @@ Partial Class InicioSesion
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(InicioSesion))
         Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.txtNombreUsuario = New System.Windows.Forms.TextBox()
-        Me.txtContra = New System.Windows.Forms.TextBox()
         Me.btnIngresar = New System.Windows.Forms.Button()
+        Me.txtContra = New System.Windows.Forms.TextBox()
+        Me.txtNombreUsuario = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Panel1
         '
-        Me.Panel1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
         Me.Panel1.Controls.Add(Me.btnIngresar)
         Me.Panel1.Controls.Add(Me.txtContra)
         Me.Panel1.Controls.Add(Me.txtNombreUsuario)
         Me.Panel1.Controls.Add(Me.Label3)
         Me.Panel1.Controls.Add(Me.Label2)
         Me.Panel1.Controls.Add(Me.Label1)
-        Me.Panel1.Location = New System.Drawing.Point(-1, -1)
+        Me.Panel1.Location = New System.Drawing.Point(0, -1)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(690, 330)
+        Me.Panel1.Size = New System.Drawing.Size(400, 465)
         Me.Panel1.TabIndex = 0
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("MS Reference Sans Serif", 14.25!, System.Drawing.FontStyle.Bold)
-        Me.Label1.ImeMode = System.Windows.Forms.ImeMode.NoControl
-        Me.Label1.Location = New System.Drawing.Point(209, 31)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(208, 24)
-        Me.Label1.TabIndex = 2
-        Me.Label1.Text = "INICIO DE SESIÓN"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!)
-        Me.Label2.ImeMode = System.Windows.Forms.ImeMode.NoControl
-        Me.Label2.Location = New System.Drawing.Point(57, 98)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(125, 16)
-        Me.Label2.TabIndex = 4
-        Me.Label2.Text = "Nombre de usuario:"
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!)
-        Me.Label3.ImeMode = System.Windows.Forms.ImeMode.NoControl
-        Me.Label3.Location = New System.Drawing.Point(57, 169)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(79, 16)
-        Me.Label3.TabIndex = 5
-        Me.Label3.Text = "Contraseña:"
-        '
-        'txtNombreUsuario
-        '
-        Me.txtNombreUsuario.Location = New System.Drawing.Point(213, 94)
-        Me.txtNombreUsuario.Name = "txtNombreUsuario"
-        Me.txtNombreUsuario.Size = New System.Drawing.Size(248, 20)
-        Me.txtNombreUsuario.TabIndex = 6
-        '
-        'txtContra
-        '
-        Me.txtContra.Location = New System.Drawing.Point(213, 169)
-        Me.txtContra.Name = "txtContra"
-        Me.txtContra.Size = New System.Drawing.Size(248, 20)
-        Me.txtContra.TabIndex = 7
         '
         'btnIngresar
         '
-        Me.btnIngresar.Location = New System.Drawing.Point(256, 241)
+        Me.btnIngresar.Location = New System.Drawing.Point(118, 314)
         Me.btnIngresar.Name = "btnIngresar"
         Me.btnIngresar.Size = New System.Drawing.Size(151, 38)
         Me.btnIngresar.TabIndex = 8
         Me.btnIngresar.Text = "INGRESAR"
         Me.btnIngresar.UseVisualStyleBackColor = True
         '
+        'txtContra
+        '
+        Me.txtContra.Location = New System.Drawing.Point(165, 229)
+        Me.txtContra.Name = "txtContra"
+        Me.txtContra.Size = New System.Drawing.Size(190, 20)
+        Me.txtContra.TabIndex = 7
+        '
+        'txtNombreUsuario
+        '
+        Me.txtNombreUsuario.Location = New System.Drawing.Point(165, 142)
+        Me.txtNombreUsuario.Name = "txtNombreUsuario"
+        Me.txtNombreUsuario.Size = New System.Drawing.Size(190, 20)
+        Me.txtNombreUsuario.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!)
+        Me.Label3.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.Label3.Location = New System.Drawing.Point(70, 233)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(79, 16)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Contraseña:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!)
+        Me.Label2.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.Label2.Location = New System.Drawing.Point(34, 142)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(125, 16)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Nombre de usuario:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("MS Reference Sans Serif", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.Label1.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.Label1.Location = New System.Drawing.Point(95, 54)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(208, 24)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "INICIO DE SESIÓN"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(394, -1)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(465, 465)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
         'InicioSesion
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(688, 326)
+        Me.ClientSize = New System.Drawing.Size(856, 463)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Panel1)
         Me.Name = "InicioSesion"
         Me.Text = "InicioSesion"
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -123,4 +137,5 @@ Partial Class InicioSesion
     Friend WithEvents txtContra As TextBox
     Friend WithEvents txtNombreUsuario As TextBox
     Friend WithEvents Label3 As Label
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
